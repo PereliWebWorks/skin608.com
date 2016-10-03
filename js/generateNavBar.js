2aceb77516149b182d6d66f0aa780acac08c3da4
Andrew Pereli <drewpereli@gmail.com>
2016-10-03 17:28:21 -0500

    added some shit

--- js/generateNavBar.js
+++ js/generateNavBar.js
@@ -8,7 +8,6 @@ var stringToWrite = `
 		    </div>
 			<div class="collapse navbar-collapse">
 				<ul class="nav navbar-nav">
-					<li><a href="about.html">About Us</a></li>
 					<li><a href="services.html">Services and Prices</a></li>
 					<li><a href="contact.html">Contact Us</a></li>
 				</ul>
@@ -23,21 +22,30 @@ var beginning = `
 	<nav class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">
 			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 				<a class="navbar-brand" href="index.html">Home</a>
 		    </div>
-			<div class="collapse navbar-collapse">
+			<div class="collapse navbar-collapse" id="navbar-collapse">
 				<ul class="nav navbar-nav">`;
+
+
 var end = `</ul>
 			</div>
 		</div>
 	</nav>
 	`;
+
+
 var middle;
 if (pagename === "index")
 {
 	middle = 
 	`
-		<li><a href="about.html">About Us</a></li>
 		<li><a href="services.html">Services and Prices</a></li>
 		<li><a href="contact.html">Contact Us</a></li>
 	`;
@@ -46,17 +54,7 @@ else if (pagename === "services")
 {
 	middle = 
 	`
-		<li><a href="about.html">About Us</a></li>
 		<li class="active"><a href="services.html">Services and Prices</a></li>
-		<li><a href="contact.html">Contact Us</a></li>
-	`;
-}
-else if (pagename === "about")
-{
-	middle = 
-	`
-		<li class="active"><a href="about.html">About Us</a></li>
-		<li><a href="services.html">Services and Prices</a></li>
 		<li><a href="contact.html">Contact Us</a></li>
 	`;
 }
@@ -64,7 +62,6 @@ else if (pagename === "contact")
 {
 	middle = 
 	`
-		<li><a href="about.html">About Us</a></li>
 		<li><a href="services.html">Services and Prices</a></li>
 		<li class="active"><a href="contact.html">Contact Us</a></li>
 	`;
